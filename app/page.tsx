82d73189f6be4d0d46f4105b667aa042c3c12f31
Richard Griffiths <rich.griffiths89@gmail.com>
2023-08-23 09:25:47 +0100

    moved axios into client

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import { ProjectList } from "./projects/components/Projects";
 
 
 export default async function Projects() {
-  const { data } = await axios.get(`${process.env.API_URL}/api/projects`)
+  // const { data } = await axios.get(`${process.env.API_URL}/api/projects`)
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProjectList data={data} />
+        <ProjectList />
       </div>
     </div>
   )
--- app/projects/components/Projects.tsx
+++ app/projects/components/Projects.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { FC, useMemo, useState } from 'react'
+import React, { FC, useEffect, useMemo, useState } from 'react'
 import { Heading } from '../../../components/Heading'
 import { Separator } from '../../../components/ui/separator'
 import { useRouter } from 'next/navigation'
@@ -12,14 +12,28 @@ import { Calendar } from 'lucide-react'
 import Link from 'next/link'
 import { ProjectCard } from './project-card'
 import { Input } from '@/components/ui/input'
+import axios from 'axios'
 
 interface ProjectsProps {
-    data: Project[]
 }
 
-export const ProjectList: FC<ProjectsProps> = ({ data }) => {
+export const ProjectList: FC<ProjectsProps> = ({ }) => {
     const router = useRouter();
     const [searchTerm, setSearchTerm] = useState('')
+    const [data, setData] = useState<Project[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+
+    const fetchProjects = async () => {
+        const projects = await axios.get(`/api/projects`)
+        return projects
+    }
+
+    useEffect(() => {
+        fetchProjects().then(result => {
+            setData(result.data)
+            setIsLoading(false)
+        })
+    }, [])
 
     function getFilteredList() {
         console.log(searchTerm)
@@ -38,21 +52,28 @@ export const ProjectList: FC<ProjectsProps> = ({ data }) => {
             </div>
             <Separator />
 
-            <div id='data-grid' className='hidden md:block'>
-                <DataTable columns={columns} data={data} />
-            </div>
+            {isLoading ? (
+                <p>Loading Projects...</p>
+            ) : (
+                <>
+                <div id='data-grid' className='hidden md:block'>
+                    <DataTable columns={columns} data={data} />
+                </div>
 
-            <div id='card-grid' className='flex flex-col justify-start items-start gap-4 md:hidden'>
-                <Input
-                    placeholder="Search By Name"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    className="max-w-full"
-                />
-                {filteredList && filteredList.map((project:Project) => (
-                    <ProjectCard key={project.id} project={project} />
-                ))}
-            </div>
+                <div id='card-grid' className='flex flex-col justify-start items-start gap-4 md:hidden'>
+                    <Input
+                        placeholder="Search By Name"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="max-w-full"
+                    />
+                    {filteredList && filteredList.map((project:Project) => (
+                        <ProjectCard key={project.id} project={project} />
+                    ))}
+                </div>
+                </>
+            )}
+            
         </>
     )
 }
