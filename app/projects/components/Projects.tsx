df39de6c99671b1a67973732a339a10aa9729b0b
Richard Griffiths <rich.griffiths89@gmail.com>
2023-08-24 11:04:52 +0100

    old routing method added

--- app/projects/components/Projects.tsx
+++ app/projects/components/Projects.tsx
@@ -24,20 +24,54 @@ export const ProjectList: FC<ProjectsProps> = ({ }) => {
     const [searchTerm, setSearchTerm] = useState('')
     const [data, setData] = useState<Project[]>([])
     const [isLoading, setIsLoading] = useState<boolean>(true)
+    
+    const fetchData = async () => {
+        let completed = false;
+        let page_number = 1;
+        let projects = []
+        let all_projects: any[] = []
 
-    const fetchProjects = async () => {
-        const response = await fetch(`/api/projects`);
+        while (!completed) {
+            try {
+                projects = await fetchProjects(page_number)
+                projects.map((project: Project) => all_projects.push(project))
+                const fetchMore = await CheckProjectCount(projects)
+                if(fetchMore) {
+                    page_number = page_number + 1
+                    projects = await fetchProjects(page_number)
+                } else {
+                    completed = true
+                }
+            } catch (error) {
+                console.log(`Error ${error}`)
+            }
+        }
+        
+        const visibleProjects = all_projects.filter(x => x.custom_fields.visible_to_business == 'Yes')
+        return visibleProjects
+    }
+
+    const fetchProjects = async (page_number: number) => {
+        const response = await fetch(`/api/projects?page=${page_number}`, { cache: 'no-store' });
         const result = await response.json();
         return result
-        // const projects = await axios.get(`/api/projects`)
-        // return projects
+    }
+
+    const CheckProjectCount = async (projects:any) => {
+        if(projects.length === 10) {
+            return true
+    
+        } else if(projects.length <= 10) {
+            return false
+        }
     }
 
     useEffect(() => {
-        fetchProjects().then(result => {
-            setData(result)
-            setIsLoading(false)
-        })
+        // fetchProjects().then(result => {
+        //     setData(result)
+        //     setIsLoading(false)
+        // })
+        fetchData().then(result => console.log(result))
     }, [])
 
     function getFilteredList() {
